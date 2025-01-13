b6308c0db85c0e197d17deafb43a69060ef8b83f
danielxit1s <ruanzindograu062@gmail.com>
2025-01-13 01:31:24 -0300

    Delete www.thunderanticheat.com directory

--- www.thunderanticheat.com/_next/static/chunks/app/(main)/layout-9ffe3fbf4584ed57.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4095,4760],{6275:function(n,e,u){Promise.resolve().then(u.t.bind(u,5250,23))}},function(n){n.O(0,[5250,2971,8069,1744],function(){return n(n.s=6275)}),_N_E=n.O()}]);
